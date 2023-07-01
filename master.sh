27a4c2bb54bd94f8d8e0e407767dba1a9a10c42f
valdir-alves3000 <valdiralves3000@gmail.com>
2023-07-01 17:41:39 -0300

    add one more machine to the cluster and configuring a fixed ip

--- master.sh
+++ master.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 sudo docker swarm init --advertise-addr=10.10.10.100
 sudo docker swarm join-token worker | grep docker > /vagrant/worker.sh
+
+sudo docker volume create info_volume
+
+sudo bash -c 'echo "<?php phpinfo(); ?>" > /var/lib/docker/volumes/info_volume/_data/info.php'
+
+sudo cp /var/lib/docker/volumes/info_volume/_data/info.php /vagrant/
+sudo cp /vagrant/worker.sh /vagrant/info.php /vagrant/
+
+sudo docker run -d -p 80:80 -v info_volume:/var/www/html --name httpd-container httpd
